2d1098f6855112ef184628beb3c91bdfc93812ec
Andyveloper <mcantillo.gdi@gmail.com>
2022-11-24 11:36:51 -0500

    Single file component basic syntax

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,19 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+const name = "Dynamic Vue";
+const styleColor = "color: blue";
+const arrayColor = ["blue", "green", "red"];
+const active = true;
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello {{ name.toUpperCase() }}</h1>
+  <h2 :style="`color: ${arrayColor[1]}`">{{ arrayColor }}</h2>
+  <h2 :style="styleColor">I'm blue</h2>
+  <h2>{{ active ? "I'm active" : "I'm not active" }}</h2>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+h1 {
+  color: red;
 }
 </style>
