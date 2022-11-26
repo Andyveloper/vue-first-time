d46d5746dbd0339d4929a4b346556e43039d8453
Andyveloper <mcantillo.gdi@gmail.com>
2022-11-26 14:36:33 -0500

    Refactor to computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 const name = "Dynamic Vue 3";
 const counter = ref(0);
 const increment = () => {
@@ -12,19 +12,23 @@ const decrement = () => {
 const reset = () => {
   counter.value = 0;
 };
+
+const classCounter = computed(() => {
+  if (counter.value < 0) {
+    return "negative";
+  } else if (counter.value > 0) {
+    return "positive";
+  } else {
+    return "zero";
+  }
+});
 </script>
 
 <template>
   <main @click.right.prevent>
     <h1>Welcome to {{ name }}</h1>
-    <div v-if="counter < 0">
-      <h2 :class="'negative'">{{ counter }}</h2>
-    </div>
-    <div v-else-if="counter > 0">
-      <h2 :class="'positive'">{{ counter }}</h2>
-    </div>
-    <div v-else>
-      <h2>{{ counter }}</h2>
+    <div>
+      <h2 :class="classCounter">{{ counter }}</h2>
     </div>
     <button @click="increment">Increment</button>
     <button @click="decrement">Decrement</button>
@@ -39,5 +43,9 @@ const reset = () => {
 
 .negative {
   color: red;
+}
+
+.zero {
+  color: peru;
 }
 </style>
